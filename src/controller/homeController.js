b1955c07278ffe34187937351d4065e2d41e88aa
Bui Van Thai <buithaild230899@gmail.com>
2022-06-20 15:24:53 +0700

    connect DB

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,6 +1,23 @@
+import connection from '../configs/connectDB';
+
 let getHomePage = (req, res) => {
-    //logic
-    return res.render('test/index.ejs')
+    // logic
+    // simple query
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+            });
+            return res.render('test/index.ejs', { dataUser: JSON.stringify(data) })
+        });
+
 }
 
 module.exports = {
--- src/route/web.js
+++ src/route/web.js
@@ -4,6 +4,7 @@ import homeController from '../controller/homeController';
 let router = express.Router();
 
 const initWebRoute = (app) => {
+
     router.get('/', homeController.getHomePage);
     router.get('/about', (req, res) => {
         res.send(`I am Thai Bui`)
