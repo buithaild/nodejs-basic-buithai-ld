ad4601e956e9981461693fed958f148d055bfc0e
Bui Van Thai <buithaild230899@gmail.com>
2022-06-21 20:26:41 +0700

    CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,27 +1,41 @@
 import pool from '../configs/connectDB';
-
-let getHomePage = async (req, res) => {
-
-    const [rows, fields] = await pool.execute('SELECT * FROM users ');
-
-    return res.render('index.ejs', { dataUser: rows })
+let getHomepage = async (req, res) => {
+    const [rows, fields] = await pool.execute('SELECT * FROM users');
+    return res.render('index.ejs', { dataUser: rows, test: 'abc string test' })
 }
-
 let getDetailPage = async (req, res) => {
     let userId = req.params.id;
-    let [user] = await pool.execute(`select * from users where id = ? `, [userId]);
-
-    return res.send(JSON.stringify(user));
+    let [user] = await pool.execute(`select * from users where id = ?`, [userId]);
+    return res.send(JSON.stringify(user))
 }
-
-let creatNewUser = async (req, res) => {
-    console.log('check req: ', req.body)
+let createNewUser = async (req, res) => {
     let { firstName, lastName, email, address } = req.body;
-    await pool.execute('insert into users(firstName, lastName, email, address) values(?, ?, ?, ?)',
+    await pool.execute('insert into users(firstName, lastName, email, address) values (?, ?, ?, ?)',
         [firstName, lastName, email, address]);
     return res.redirect('/')
 }
 
-module.exports = {
-    getHomePage, getDetailPage, creatNewUser
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('delete from users where id = ?', [userId])
+    return res.redirect('/');
 }
+
+let getEditPage = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('Select * from users where id = ?', [id]);
+    return res.render('update.ejs', { dataUser: user[0] }); // x <- y
+}
+
+let postUpdateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+
+    await pool.execute('update users set firstName= ?, lastName = ? , email = ? , address= ? where id = ?',
+        [firstName, lastName, email, address, id]);
+
+    return res.redirect('/');
+}
+
+module.exports = {
+    getHomepage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
+} 
--- src/route/web.js
+++ src/route/web.js
@@ -1,19 +1,19 @@
 import express from "express";
-import pool from "../configs/connectDB";
 import homeController from '../controller/homeController';
-
 let router = express.Router();
-
 const initWebRoute = (app) => {
+    router.get('/', homeController.getHomepage);
+    router.get('/detail/user/:id', homeController.getDetailPage);
+    router.post('/create-new-user', homeController.createNewUser);
 
-    router.get('/', homeController.getHomePage);
-    router.get('/detail/user/:id', homeController.getDetailPage)
-    router.post('/create-new-user', homeController.creatNewUser)
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditPage);
+    router.post('/update-user', homeController.postUpdateUser)
+
+
     router.get('/about', (req, res) => {
-        res.send(`I am Thai Bui`)
+        res.send(`I'm Eric!`)
     })
-
     return app.use('/', router)
 }
-
 export default initWebRoute;
