92a5f4b522abfa267009fa084a3e11f0b85c910b
Bui Van Thai <buithaild230899@gmail.com>
2022-06-21 11:21:39 +0700

    creat a new user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -14,6 +14,14 @@ let getDetailPage = async (req, res) => {
     return res.send(JSON.stringify(user));
 }
 
+let creatNewUser = async (req, res) => {
+    console.log('check req: ', req.body)
+    let { firstName, lastName, email, address } = req.body;
+    await pool.execute('insert into users(firstName, lastName, email, address) values(?, ?, ?, ?)',
+        [firstName, lastName, email, address]);
+    return res.redirect('/')
+}
+
 module.exports = {
-    getHomePage, getDetailPage
+    getHomePage, getDetailPage, creatNewUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -8,6 +8,7 @@ const initWebRoute = (app) => {
 
     router.get('/', homeController.getHomePage);
     router.get('/detail/user/:id', homeController.getDetailPage)
+    router.post('/create-new-user', homeController.creatNewUser)
     router.get('/about', (req, res) => {
         res.send(`I am Thai Bui`)
     })
