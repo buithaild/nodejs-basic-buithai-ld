071470f28da94763c5b6f3a5e92cf67e1d33efc5
Bui Van Thai <buithaild230899@gmail.com>
2022-06-22 23:38:53 +0700

    middle ware

--- src/server.js
+++ src/server.js
@@ -4,10 +4,21 @@ import configViewEngine from './configs/viewEngine';
 import initWebRoute from './route/web';
 import initAPIRoute from './route/api'
 // import connection from './configs/connectDB';
+
 require('dotenv').config();
+var morgan = require('morgan')
+
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use((req, res, next) => {
+    // check => return.send()
+    console.log('>>> run into my middleware')
+    console.log(req.method)
+    next();
+})
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +31,11 @@ initWebRoute(app);
 
 //init api route
 initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
